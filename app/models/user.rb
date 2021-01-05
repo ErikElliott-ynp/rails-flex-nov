652ed25b1c5fa1344f3f74913bd015f6b7e4ff7c
erikelliott-ynp <erikelliott92@gmail.com>
2021-01-04 18:05:09 -0800

    Add SPIRE

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,57 @@
 class User < ApplicationRecord
-    validates :username, :email, presence: true
-    validates :username, uniqueness: true
+    validates :email, :password_digest, presence: true
+    validates :username, :session_token, presence: true, uniqueness: true
+    validates :password, length: { minimum: 6 },allow_nil: true
+
+    attr_reader :password
+    # attr_reader is used in the validation of the password
+    
+    after_initialize :ensure_session_token
+    # before_validation :ensure_session_token
+    # set the session_token using the ensure_session_token method
+
+    # S - self.find_by_credentials
+    # P - password=
+    # I - is_password?
+    # R - reset_session_token!
+    # E - ensure_session_token
+
+    def self.find_by_credentials(username, password)
+        user = User.find_by(username: username)
+        # find the user by their username
+        user && user.is_password?(password) ? user : nil
+        # return nil is the find_by method returns nil
+        # use the User#is_password? method to compare the password give with the password_digest from the DB
+        # return the user is they match or return nil
+    end
+
+    def password=(password)
+        self.password_digest = BCrypt::Password.create(password)
+        # create the hashed/salted password_digest from the given password using BCrypt
+        @password = password
+        # set an instance variable for use in the validation
+    end
+
+    def is_password?(password)
+        BCrypt::Password.new(self.password_digest).is_password?(password)
+        # create a new instance of the BCrypt::Password class with the password_digest for this instance
+        # then call the BCrypt::Password#is_password? method to compare the given password
+        # (this method does not use any recursion)
+    end
+
+    def reset_session_token!
+        self.session_token = SecureRandom::urlsafe_base64
+        # set the session token to a new random string
+        self.save!
+        # .save! throws and errow and stops code execution= => easier to debug
+        self.session_token
+        # return the value of the session token for use later
+    end
+
+    def ensure_session_token
+        self.session_token ||= SecureRandom::urlsafe_base64
+        # makes sure the user has a session_token when first created
+        # the ||= is important later for logging in!
+        
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_30_010857) do
+ActiveRecord::Schema.define(version: 2021_01_05_011529) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,9 @@ ActiveRecord::Schema.define(version: 2020_12_30_010857) do
     t.boolean "is_cool?", default: true
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest", null: false
+    t.string "session_token", null: false
+    t.index ["session_token"], name: "index_users_on_session_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
